67a6dd2252ebd366302e4617b46928a920f89783
matheusbach <math.bach.com@gmail.com>
2021-06-20 16:17:34 -0300

    v1.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,60 +1,67 @@
-﻿using Microsoft.Win32;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 
-
-namespace get_files_hash_checksum
+namespace FileHasher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class FileHasher : Window
     {
-        public MainWindow()
+        public static configs config { get; set; } = new();
+
+        public FileHasher()
         {
             InitializeComponent();
-            DataContext = this;
+            DataContext = config;
         }
 
-        public bool startPathCkecked { get; set; } = false;
-        public bool startDlLinkCkecked { get; set; } = false;
-        public string startPath { get; set; } = @"C:\";
-        public string startDlLink { get; set; } = @"https://";
-        public OpenFileDialog selectedFiles { get; set; } = null;
-        public string stringResult { get; set; } = "Select files to show result";
+        public void GuiUpdater()
+        {
+            textBoxResult.Text = config.stringResult;
+            textStartPath.Text = config.startPath;
+            textStartDlLink.Text = config.startDlLink;
+        }
 
+        private CommonOpenFileDialog openFileDialog = new CommonOpenFileDialog() { Multiselect = true, AllowNonFileSystemItems = true, EnsureFileExists = true, EnsurePathExists = true, EnsureReadOnly = true, EnsureValidNames = true, IsFolderPicker = true, NavigateToShortcut = true, ShowHiddenItems = true, ShowPlacesList = true, Title = "Select what you want to checksum", InitialDirectory = config.startPathCkecked && Directory.Exists(config.startPath) ? config.startPath : null };
 
         private void btnGetDirectory_Click(object sender, RoutedEventArgs e)
         {
+            if (openFileDialog.ShowDialog() != CommonFileDialogResult.Ok)
+            {
+                MessageBox.Show("Nothing selected");
+                return;
+            }
+
             ListOfFiles.Clear();
 
-            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true, CheckFileExists = true, CheckPathExists = true, Title = "Select what you want to checksum", InitialDirectory = startPathCkecked && Directory.Exists(startPath) ? startPath : null};
-            if (openFileDialog.ShowDialog() == true)
+            config.stringResult = "Hashing all files. The process may take some time and the screen may freeze in the meantime. hold...";
+            GuiUpdater();
+
+            foreach (string filePath in openFileDialog.FileNames)
             {
-                selectedFiles = openFileDialog;
-
-                foreach(string filePath in openFileDialog.FileNames)
+                MessageBox.Show(JsonConvert.SerializeObject(openFileDialog.FileNames));
+                if (File.Exists(filePath))
                 {
-                    ListOfFiles.Add(new FileParameters() { DlLink = startDlLinkCkecked && Uri.IsWellFormedUriString(startDlLink, UriKind.Absolute) && File.Exists(filePath) && startPathCkecked && Directory.Exists(startPath) ? new Uri(new Uri(startDlLink, UriKind.Absolute), Path.GetRelativePath(startPath + '/', filePath), false).AbsoluteUri : null, FileName = Path.GetFileName(filePath), Path = startPathCkecked ? Directory.Exists(startPath) ? Path.GetRelativePath(startPath, filePath).Replace(@"//", @"/").Replace(@"\\", @"\").Replace(@"\", @"/") : null : Directory.Exists(startPath) ? Path.GetFullPath(filePath) : null, Sha256 = File.Exists(filePath) ? FileSHA256(filePath) : null }); ;
+                    ListOfFiles.Add(new FileParameters() { DlLink = config.startDlLinkCkecked && Uri.IsWellFormedUriString(config.startDlLink, UriKind.Absolute) && File.Exists(filePath) && config.startPathCkecked && Directory.Exists(config.startPath) ? new Uri(new Uri(config.startDlLink, UriKind.Absolute), Path.GetRelativePath(config.startPath + '/', filePath), false).AbsoluteUri : null, FileName = Path.GetFileName(filePath), Path = config.startPathCkecked ? Directory.Exists(config.startPath) ? Path.GetRelativePath(config.startPath, filePath).Replace(@"//", @"/").Replace(@"\\", @"\").Replace(@"\", @"/") : null : Directory.Exists(config.startPath) ? Path.GetFullPath(filePath).Replace(@"//", @"/").Replace(@"\\", @"\").Replace(@"\", @"/") : null, Sha256 = File.Exists(filePath) ? FileSHA256(filePath) : null }); ;
                 }
-
-                textBoxResult.Text = JsonConvert.SerializeObject(ListOfFiles, Formatting.Indented);
-                MessageBox.Show(JsonConvert.SerializeObject(ListOfFiles, Formatting.Indented));
+                else if (Directory.Exists(filePath))
+                {
+                    foreach (string filePathInSubdir in Directory.GetFiles(filePath, "*", SearchOption.AllDirectories))
+                    {
+                        ListOfFiles.Add(new FileParameters() { DlLink = config.startDlLinkCkecked && Uri.IsWellFormedUriString(config.startDlLink, UriKind.Absolute) && File.Exists(filePathInSubdir) && config.startPathCkecked && Directory.Exists(config.startPath) ? new Uri(new Uri(config.startDlLink, UriKind.Absolute), Path.GetRelativePath(config.startPath + '/', filePathInSubdir), false).AbsoluteUri : null, FileName = Path.GetFileName(filePathInSubdir), Path = config.startPathCkecked ? Directory.Exists(config.startPath) ? Path.GetRelativePath(config.startPath, filePathInSubdir).Replace(@"//", @"/").Replace(@"\\", @"\").Replace(@"\", @"/") : null : Directory.Exists(config.startPath) ? Path.GetFullPath(filePathInSubdir).Replace(@"//", @"/").Replace(@"\\", @"\").Replace(@"\", @"/") : null, Sha256 = File.Exists(filePathInSubdir) ? FileSHA256(filePathInSubdir) : null }); ;
+                    }
+                }
             }
+
+            if (ListOfFiles.Count == 0) { config.stringResult = "No files in selected directories"; }
+            else
+            {
+                config.stringResult = JsonConvert.SerializeObject(ListOfFiles, Formatting.Indented);
+            }
+
+            GuiUpdater();
         }
 
         public static string FileSHA256(string filePath)
@@ -66,8 +73,8 @@ namespace get_files_hash_checksum
             }
         }
 
-
         public static List<FileParameters> ListOfFiles { get; set; } = new();
+
         public partial class FileParameters
         {
             [JsonProperty("dlLink", NullValueHandling = NullValueHandling.Ignore)]
@@ -81,6 +88,38 @@ namespace get_files_hash_checksum
 
             [JsonProperty("sha256", NullValueHandling = NullValueHandling.Ignore)]
             public string Sha256 { get; set; }
+        }
+
+        public class configs
+        {
+            public bool startPathCkecked { get; set; } = false;
+            public bool startDlLinkCkecked { get; set; } = false;
+            public string startPath { get; set; } = @"C:\";
+            public string startDlLink { get; set; } = @"https://";
+            public string stringResult { get; set; } = "Select files to show result";
+        }
+
+        private void btnGetStartDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog cfd = new CommonOpenFileDialog
+            {
+                EnsurePathExists = true,
+                EnsureFileExists = false,
+                AllowNonFileSystemItems = true,
+                Multiselect = false,
+                IsFolderPicker = true,
+                Title = "Select a Directory"
+            };
+
+            if (cfd.ShowDialog() != CommonFileDialogResult.Ok)
+            {
+                MessageBox.Show("No directory selected");
+                return;
+            }
+
+            config.startPath = Directory.Exists(cfd.FileName) ? cfd.FileName : Path.GetDirectoryName(cfd.FileName);
+
+            GuiUpdater();
         }
     }
 }
